832b3c641f7dc15b69e3b2e72526ba20b1f89f39
Harish G <gunalanharish10@gmail.com>
2023-11-10 19:05:49 +0530

    Basic To-Do List

--- script.js
+++ script.js
@@ -28,12 +28,34 @@ listContainer.addEventListener("click", function(e){
     }
 }, false);
 
+// To Stores Data
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML)
 }
 
+// To show the Stored Data
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
 
 showTask();
+
+// To display Date and Time
+function displayDateTime(){
+    var currentDate = new Date();
+    var dateString = currentDate.toDateString();
+    var timeString = currentDate.toLocaleTimeString();
+
+    document.getElementById("date").innerText = "Date: "+ dateString;
+    document.getElementById("time").innerText = "Time: "+ timeString;
+}
+
+setInterval(displayDateTime, 1000);
+
+// Use of Enter Key with Add Button
+function handleKeyPress(event){
+    if (event.keyCode === 13){
+        event.preventDefault();
+        document.getElementById("add").click();
+    }
+}
